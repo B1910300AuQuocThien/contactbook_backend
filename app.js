0532f9f120727d62d927cbb6d0122e39ad22ceb1
thien2101 <thienb1910300@student.ctu.edu.vn>
2023-02-06 20:46:49 +0700

    dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const cors = require("cors")
-
+const contactsRouter = require("./app/routers/contact.route")
 const app = express()
 
+app.use("/api/contacts", contactsRouter)
 app.use(cors())
 app.use(express.json())
 app.get("/", (req, res) => {
