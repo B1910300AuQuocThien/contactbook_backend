ebe590c7443b68fc8f627089ea15b5faf05baf91
thien2101 <thienb1910300@student.ctu.edu.vn>
2023-02-07 09:47:53 +0700

    cai dat thu vien mongodb, dinh nghia ham tro giup ket nooi

---
+++ app/services/contact.service.js
@@ -0,0 +1,8 @@
+const { ObjectID } = require("mongodb")
+class ContactService {
+    constructor(client) {
+        this.Contact = client.db().collection("contacts")
+    }
+}
+
+module.exports = ContactService
